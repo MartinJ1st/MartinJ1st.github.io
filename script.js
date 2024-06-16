fc079b485fc038be53c320505fd1497d158c7814
Мартин Јанев <150262130+MartinJ1st@users.noreply.github.com>
2024-06-16 17:26:17 +0200

    git commit

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-function toggleMenu(){
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-}
-
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
