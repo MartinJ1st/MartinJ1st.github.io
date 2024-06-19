349c28a02cb804291f420ff7478bb08d6632d749
Мартин Јанев <150262130+MartinJ1st@users.noreply.github.com>
2024-06-19 16:18:17 +0200

    git commit

--- script.js
+++ script.js
@@ -3,6 +3,30 @@ function topFunction() {
     document.documentElement.scrollTop = 0;
 }
 
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("desktop-nav");
+var sticky = header.offsetTop;
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+        if (window.pageYOffset > 100) {
+            header.classList.add("glue"); // Add the "glue" class
+            header.style.backgroundColor = "#3dca00";
+            header.style.transition = "background-color 0.5s, color 0.5s";
+            header.style.zIndex = "9999";
+        }
+    } else {
+        header.classList.remove("sticky");
+        header.classList.remove("glue"); // Remove the "glue" class
+        header.style.backgroundColor = "";
+        header.style.color = "";
+        header.style.transition = "background-color 0.5s, color 0.5s";
+        header.style.zIndex = ""; 
+    }
+}
+window.onscroll = function() {myFunction()};
+
 function send(){
     var templateParams = 
     {
