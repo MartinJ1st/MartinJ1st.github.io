f42d224f9aafd065f1b98217efbfcf52d35bc469
Мартин Јанев <150262130+MartinJ1st@users.noreply.github.com>
2024-09-22 00:18:34 +0200

    git commit

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-function topFunction() {
+function goToTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
 
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {
+    updateHeaderOnScroll();
+};
 
 var header = document.getElementById("desktop-nav");
 var sticky = header.offsetTop;
-function myFunction() {
+
+function updateHeaderOnScroll() {
     if (window.pageYOffset > sticky) {
         header.classList.add("sticky");
-        if (window.pageYOffset > 100) {
+        if (window.pageYOffset > 80) {
             header.classList.add("glue"); // Add the "glue" class
             header.style.backgroundColor = "#3dca00";
             header.style.transition = "background-color 0.5s, color 0.5s";
@@ -22,10 +25,31 @@ function myFunction() {
         header.style.backgroundColor = "";
         header.style.color = "";
         header.style.transition = "background-color 0.5s, color 0.5s";
-        header.style.zIndex = ""; 
+        header.style.zIndex = "";
     }
-
 }
-window.onscroll = function() {myFunction()};
-            
-    
+
+window.onscroll = function() {
+    updateHeaderOnScroll();
+};
+
+const hamburger = document.getElementById('hamburger');
+const menu = document.getElementById('menu');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
+
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        const offsetPosition = targetElement.offsetTop - 200;
+
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    });
+});
